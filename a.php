f2101dded7d23e8dedf3c4efc6d889b393c07e8b
DESKTOP-M14PJVA\User <malith124@gmail.com>
2018-02-04 01:58:09 +0530

    Contact Us Bug 0.1

--- a.php
+++ a.php
@@ -1,13 +1,20 @@
 <?php
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$pnum = $_POST['pnum'];
-$message = $_POST['subject'];
-$formcontent="From: $name $lname $pnum \n Message: $message";
-$recipient = "malith124@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+if(isset($_POST['submit'])){
+    $to = "malith.fernando@revport.net"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+	$phone = $_POST['phone'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " " . $phone . " " . $from . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+   	header('Location: http://ascentic.revportx.com');
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
