856e43c03204005757394119783e15bfd04c3f1b
DESKTOP-M14PJVA\User <malith124@gmail.com>
2018-02-04 21:19:04 +0530

    Spelling

--- a.php
+++ a.php
@@ -14,7 +14,6 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    print "Thank You for Message Us";
    	header('Location: http://localhost/MobileUIUX/home.html');
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
